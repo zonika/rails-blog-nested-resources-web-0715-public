f29ff24f8247c8bf578263be99e02b16b9fbe8ea
zonika <zchodosh@conncoll.edu>
2015-09-03 13:09:47 -0400

    done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
-
+  belongs_to :post
+  belongs_to :user
+  validates :content, presence:true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,14 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
   accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
   validates_presence_of :name, :content
+  has_many :comments
+
+  after_destroy :delete_comments
+
+  private
+  def delete_comments
+    comments.each do |comment|
+      comment.destroy
+    end
+  end
 end
